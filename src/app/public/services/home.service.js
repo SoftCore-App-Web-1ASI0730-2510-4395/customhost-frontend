cd67b22f1b31e5a4b865e0a978400a30650175f2
nOOmzzzz <123845160+nOOmzzzz@users.noreply.github.com>
2025-05-14 12:03:43 -0500

    fix: add api url to home service.

--- src/app/public/services/home.service.js
+++ src/app/public/services/home.service.js
@@ -1,7 +1,7 @@
 // src/services/home.service.js
 import axios from 'axios'
 
-const API_URL = 'http://localhost:3001'
+const API_URL = 'https://api-chafa.vercel.app/api/v1/'
 
 export const getHotelDashboardStats = async () => {
     try {

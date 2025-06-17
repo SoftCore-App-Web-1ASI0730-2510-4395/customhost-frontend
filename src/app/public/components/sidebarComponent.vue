e56834e59e6d4832280a7eb2fa650143657e87df
Alison Arrieta <alisonarrieta06@gmail.com>
2025-06-17 04:30:58 -0500

    fix

--- src/app/public/components/sidebarComponent.vue
+++ src/app/public/components/sidebarComponent.vue
@@ -35,8 +35,6 @@ export default {
           icon: 'pi pi-building',
         },
         {
-          name: this.$t('sidebar_items.iot-devices'),
-          path: '/guest-experience/iot-devices',
           name: this.$t('sidebar_items.staff-devices'),
           path: '/guest-experience/staff-devices',
           type: 'staff',

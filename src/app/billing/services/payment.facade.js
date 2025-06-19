18b5e9e4d5d1a4c1514ecb88b6a125b5b72d9e35
Alison Arrieta <alisonarrieta06@gmail.com>
2025-06-18 21:18:44 -0500

    feat(payment-facade): add new functions to payment facade.

--- src/app/billing/services/payment.facade.js
+++ src/app/billing/services/payment.facade.js
@@ -3,7 +3,7 @@
 import { getUserById } from '../../profiles/services/user.service.js';
 import { getRoomById } from '../../crm/services/rooms.service.js';
 import { getHotelById } from '../../crm/services/hotels.service.js';
-import { createPayment } from './payment.service.js';
+import { createPayment, getAllPayments } from './payment.service.js';
 
 // 👇 Importa servicio de bookings
 import { createBooking } from '../../crm/services/booking.service.js';
@@ -107,5 +107,13 @@ export default {
             console.error('Error marcando habitación como ocupada:', error);
             throw error;
         }
+    },
+
+    /**
+     * Obtiene todos los pagos
+     * @returns {Promise<Array>} - Lista de pagos
+     */
+    async getAllPayments() {
+        return getAllPayments();
     }
 };
